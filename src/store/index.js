c92ad6ead8d2e1818d109189b0609e9ab62217a0
Hamid Solih <58102695+Khamid-S@users.noreply.github.com>
2021-12-18 14:21:07 +0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -26,7 +26,7 @@ export default new Vuex.Store({
   actions: {
     getCountries({ commit }){
       axios
-      .get('https://restcountries.eu/rest/v2/all')
+      .get('https://restcountries.com/v3.1/all')
       .then(response => { 
       commit('SET_COUNTRIES', response.data);
       });
